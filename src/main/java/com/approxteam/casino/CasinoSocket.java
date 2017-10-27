3068eaf61241e1251c52a5b6eda6f0dd8e3cda90
Adam Raźniewski <adam.razniewski@gmail.com>
2017-10-27 18:31:33 +0200

    Updated packages, established connection between server and client

--- src/main/java/com/approxteam/casino/CasinoSocket.java
+++ src/main/java/com/approxteam/casino/CasinoSocket.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.approxteam.antcolosseumserver;
+package com.approxteam.casino;
 
 import javax.ejb.Singleton;
 import javax.enterprise.context.ApplicationScoped;
@@ -13,6 +13,8 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -23,20 +25,24 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/casino")
 public class CasinoSocket {
     
+    private static final Logger log = LogManager.getLogger(CasinoSocket.class);    
+    
     @OnOpen
-        public void open(Session session) {
-            
+    public void open(Session session) {
+        log.info(session.getId() + " connected to casino (+)");
     }   
     @OnClose
-        public void close(Session session) {
+    public void close(Session session) {
+        log.info(session.getId() + " leaving casino (-)");
     }   
 
     @OnError
-        public void onError(Throwable error) {
-            
+    public void onError(Throwable error) {
+        log.error(error);
     }
 
     @OnMessage
     public void handleMessage(String message, Session session) {
+        log.info(session.getId() + ": " + message);
     }
 }
--- src/main/java/com/approxteam/casino/entities/AccountActivation.java
+++ src/main/java/com/approxteam/casino/entities/AccountActivation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.approxteam.antcolosseumserver.entities;
+package com.approxteam.casino.entities;
 
 import java.io.Serializable;
 import java.util.Date;
